32fc54db657e4f8e0e8df3c26561b230de49d882
shoebabedin <shoeb.abedin@intelli.global>
2024-02-01 18:58:10 +0600

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import EmailEditor from "react-email-editor";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+  const [editor, setEditor] = useState(null);
+
+  const onLoad = (editor) => {
+    setEditor(editor);
+  };
+
+  const exportHtml = () => {
+    if (editor) {
+      editor.exportHtml((data) => {
+        const { html } = data;
+        console.log("exportHtml", html);
+      });
+    }
+  };
+
+  return (
+    <>
+      <button onClick={exportHtml}>Export HTML</button>
+      <EmailEditor onLoad={onLoad} />
+    </>
+  );
 };
 
 export default Home;
